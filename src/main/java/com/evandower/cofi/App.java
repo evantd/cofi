8d00d8f9916dc5fd33556c51a874ba5299b5d75e
Evan Dower <github@evandower.com>
2017-07-23 10:31:08 -0700

    Added no-op arg parsing

--- src/main/java/com/evandower/cofi/App.java
+++ src/main/java/com/evandower/cofi/App.java
@@ -1,11 +1,36 @@
 package com.evandower.cofi;
 
+import java.io.PrintStream;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
 public class App {
+
+  private PrintStream out;
+  private PrintStream err;
+
+  public App(final PrintStream out, final PrintStream err, final String[] args) {
+    this.out = out;
+    this.err = err;
+
+    final CmdLineParser parser = new CmdLineParser(this);
+    try {
+      parser.parseArgument(args);
+    } catch (final CmdLineException e) {
+      err.println(e.getMessage());
+      parser.printUsage(err);
+    }
+  }
+
   public String getGreeting() {
     return "Hello world.";
   }
 
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+  public void run() {
+    out.println(getGreeting());
+  }
+
+  public static void main(final String[] args) {
+    new App(System.out, System.err, args).run();
   }
 }
